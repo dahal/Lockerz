dfe54464c6f3883c253b56f8d276089de9520f1a
Puru Dahal <pdahal@me.com>
2014-09-06 18:58:44 -0700

    Create and test for validations Locker_Model
    1. Availability of each lockers must be TRUE by default
    2. Each Lockers must have names
    3. Each Lockers should have unique names
    4. Locker should have names starting with either S, M, L and number between 1..1000

---
+++ app/models/locker.rb
@@ -0,0 +1,10 @@
+class Locker < ActiveRecord::Base
+  validates :name, 
+            uniqueness: true, 
+            presence: true
+            
+  validates_format_of :name, 
+  with: (/^(S|M|L)([1-9][0-9]{0,2}|1000)(,([1-9][0-9]{0,2}|1000))*$/), 
+  multiline: true, 
+  on: :create
+end
