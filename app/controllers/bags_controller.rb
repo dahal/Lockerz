28447ffc015feb3d92b79a7b7de1d4b9385e3bcd
Puru Dahal <pdahal@me.com>
2014-09-07 18:30:06 -0700

    Ajaxify create with MVC javascript design pattern

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -7,12 +7,17 @@ class BagsController < ApplicationController
   end
 
   def create
-    locker_size = params['locker']['size']
-    customer_name = params['bag']['customer_name']
+    # require "pry", binding.pry
+    locker_size = params['size']
+    customer_name = params['customer']
     @locker = Locker.where(size: locker_size, available: true).first
     @locker.update(available: false)
     @bag = Bag.create(customer_name: customer_name, 
                       locker_id: @locker.id)
+    
+    render json: {name: customer_name, 
+                  locker: @locker.name
+                  }.to_json
   end
 
 end
